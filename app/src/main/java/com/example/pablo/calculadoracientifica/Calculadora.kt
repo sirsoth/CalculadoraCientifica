89104f614be52d891c470b7d3d7b9de261bbf52d
Pablo Candela <pabcanand@hotmail.com>
2018-05-11 11:02:58 +0200

    Proyecto inicio

---
+++ app/src/main/java/com/example/pablo/calculadoracientifica/Calculadora.kt
@@ -0,0 +1,12 @@
+package com.example.pablo.calculadoracientifica
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Calculadora : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculadora)
+    }
+}
